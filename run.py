4eeab44bb98b38a3ce80409ffd719830730f390f
S-shah93 <sumair.shah@hotmail.com>
2023-01-16 13:41:21 +0000

    Add n to all inputs, for deployment to Heroku

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
