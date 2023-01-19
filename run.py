bc1d7195dd6936803f75d093fb478521c5dd901c
S-shah93 <sumair.shah@hotmail.com>
2023-01-19 00:18:22 +0000

    Add add_to_list and fix some bugs

--- run.py
+++ run.py
@@ -26,9 +26,9 @@ def interface():
         print("1: Current Rankings")
         print("2: Add movie rating to current list")
         print("3: Request a movie to be added to the list")
-        print("4: Leave comment for movie")
-        print("5: Top rated movie")
-        print("6: Lowest rated movie")
+
+        print("4: Top rated movie")
+        print("5: Lowest rated movie")
         print("0: End program")
 
         user_choice = input("Enter your choice: \n")
@@ -44,12 +44,9 @@ def interface():
             add_to_list()
             break
         elif user_choice == "4":
-            m_comment()
-            break
-        elif user_choice == "5":
             m_top()
             break
-        elif user_choice == "6":
+        elif user_choice == "5":
             m_lowest()
             break
         elif user_choice == "0":
@@ -95,12 +92,6 @@ def current_ratings():
         current_ratings()
 
 
-def m_search():
-    """
-    Function to search for movies
-    """
-
-
 def add_rating():
     """
     # Get movie review input from the user.
@@ -109,7 +100,8 @@ def add_rating():
         print("Please enter your movie review")
         print("Your review should be ten numbers, seperated by commas.")
         print("Example: 1,2,3,4,5,6,7,8,9,10\n")
-
+        print("Please input 1 number between 1 - 10 for each title\n")
+        print("Seperated by a comma ','\n")
         review_str = input("Enter your data here: \n")
 
         user_review = review_str.split(",")
@@ -120,12 +112,94 @@ def add_rating():
             update_inputs_worksheet(reviews_data)
             break
         return user_review
+    
+    print("Please see options below")
+    print("1: Add another rating")
+    print("2: See main menu again")
+    print("0: End program")
+    c_ratings_choice = input("Enter a number between 0 - 1\n")
+
+    if c_ratings_choice == "1":
+        add_rating()
+    elif c_ratings_choice == "2":
+        interface()
+    elif c_ratings_choice == "0":
+        print("Thank you for visiting!")
+        print("Goodbye")
+    else:
+        print(f"Invalid choice, you entered {c_ratings_choice}\n")
+        print("Please choose a number between 0 - 2")
+        current_ratings()
 
     print("Thank you for your review\n")
     print("Please run program again to view or review")
 
 
 def validate_rating(values):
+    """
+    # Inside the try, converts all string values into integers.
+    # Raises ValueError msg if strings cannot be converted into integers,
+    # or if there aren't exctaly 10 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 10:
+            raise ValueError(
+                f"Exactly 10 values are required, you provided {len(values)}"
+                )
+        else:
+            print(f"You provided {values}")
+    except ValueError as er:
+        print(f"Invalid data entered: {er}, please try again.\n")
+        add_rating()
+        return False
+
+    return True
+
+
+def update_inputs_worksheet(user_review):
+    """
+    # Updates inputs worksheet, add new row with the users review.
+    """
+    print("Updating inputs worksheet...\n")
+    inputs_worksheet = SHEET.worksheet("inputs")
+    inputs_worksheet.append_row(user_review)
+    print("Your review has been upoaded sucessfully.\n")
+
+
+def add_to_list():
+    """
+    Function to requests a movie to be added to the list.
+    """
+    while True:
+        print("Please enter your request below")
+        print("We will update the rankings list in the next update")
+
+        user_request = input("Enter your movie title here: \n")
+
+        print(f"You entered {user_request}")
+        if user_request is True:
+            print("Data is valid!")
+            user_title = [str(value) for value in user_request]
+            update_requests_worksheet(user_title)
+            break
+        else:
+            print("invalid data entered")
+            return True
+        return user_request
+
+
+def update_requests_worksheet(user_title):
+    """
+    Updates requests worksheet
+    """
+    print("Updating requests worksheet...\n")
+    requests_worksheet = SHEET.worksheet("requests")
+    requests_worksheet.append_row(user_title)
+    print("Your review has been upoaded sucessfully.\n")
+
+
+def validate_title(values):
     """
     # Inside the try, converts all string values into integers.
     # Raises ValueError msg if strings cannot be converted into integers,
@@ -152,35 +226,10 @@ def validate_rating(values):
     return True
 
 
-def update_inputs_worksheet(user_review):
-    """
-    # Updates inputs worksheet, add new row with the users review.
-    """
-    print("Updating inputs worksheet...\n")
-    inputs_worksheet = SHEET.worksheet("inputs")
-    inputs_worksheet.append_row(user_review)
-    print("Your review has been upoaded sucessfully.\n")
-
-
-def add_to_list():
-    """
-    Function to request a movie to be added to the list.
-    """
-    
-
-def m_comment():
-    """
-    Function allows user leave a comment for a movie
-    """
-    print("Test 5")
-
-
 def m_top():
     """
     Function to search the top rated movie
     """
-    print("Test 6")
-
     top_movie = SHEET.worksheet("highest")
     top_table = pd.DataFrame(top_movie.get_all_records())
     print(top_table.to_string(index=False))
@@ -209,8 +258,6 @@ def m_lowest():
     """
     Function to search for the lowest rated
     """
-    print("Test 7")
-
     lowest_movie = SHEET.worksheet("lowest")
     lowest_table = pd.DataFrame(lowest_movie.get_all_records())
     print(lowest_table.to_string(index=False))
